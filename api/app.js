1113dd477f0ca263cf96de3112a51ef1717f5ff8
Alashe Olawale <isiaq.oa@gmail.com>
2020-05-24 21:05:58 +0100

    refactor: Modified the project structure

--- api/app.js
+++ api/app.js
@@ -1,13 +1,13 @@
 import express from 'express';
 import morgan from 'morgan';
+import bodyParser from 'body-parser';
 import cors from 'cors';
-// import Router from './routes/index';
-
 
 const app = express();
 app.use(morgan('dev'));
 
-// const API_VERSION = '/api/v1';
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 
 app.use(cors());
@@ -18,15 +18,12 @@ app.use(
   })
 );
 
-// app.use(`${API_VERSION}`, Router);
-
-
-app.all('*', (err, req, res, next) => {
-  if (!err) return next();
-  return res.status(400).json({
-    status: 400,
-    error: `Failed to decode param: ${req.url}`
-  });
-});
+// app.all('*', (err, req, res, next) => {
+//   if (!err) return next();
+//   return res.status(400).json({
+//     status: 400,
+//     error: `Failed to decode param: ${req.url}`
+//   });
+// });
 
 export default app;
